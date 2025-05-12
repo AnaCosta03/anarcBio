7bc942757bba7a4eab353725a2dc2ac424be039d
Ana Raquel <anarc03.adm@gmail.com>
2025-05-12 17:51:48 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const b1 = document.getElementById('b1');
+const msgC = document.getElementById('msgContainer');
+
+b1.sty
+b1.addEventListener('click', mostrarMensagem);
+
+function mostrarMensagem() {
+  msgC.innerHTML = '"Conhece-te a ti mesmo e conhecerás os deuses e o universo" - Sócrates';
+}
